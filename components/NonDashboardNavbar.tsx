956a02af63169b3807f698507b09e2d16a6bb87d
monaabishek2003 <monaabisheka@gmail.com>
2025-01-03 17:12:42 +0530

    user-deatils & login buttons added in navBar feature/auth - 2

--- components/NonDashboardNavbar.tsx
+++ components/NonDashboardNavbar.tsx
@@ -1,8 +1,13 @@
+'use client'
 import React from "react";
 import Link from "next/link";
 import { BookOpen, Bell } from "lucide-react";
+import { SignedIn, SignedOut, UserButton, useUser } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const NonDashboardNavbar = () => {
+  const { user } = useUser();
+  const userRole = user?.publicMetadata?.userType as "student" | "teacher";
   return (
     <nav className="nondashboard-navbar">
       <div className="nondashboard-navbar__container">
@@ -32,6 +37,38 @@ const NonDashboardNavbar = () => {
             <span className="nondashboard-navbar__notification-indicator"></span>
             <Bell className="nondashboard-navbar__notification-icon"></Bell>
           </button>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                baseTheme: dark,
+                elements: {
+                  userButtonOuterIdentifier: "text-customgreys-dirtyGrey",
+                  userButtonBox: "scale-90 sm:scale-100",
+                },
+              }}
+              showName={true}
+              userProfileMode="navigation"
+              userProfileUrl={
+                userRole === "teacher" ? "/teacher/profile" : "/user/profile"
+              }
+            />
+          </SignedIn>
+          <SignedOut>
+            <Link
+              href="/signin"
+              className="nondashboard-navbar__auth-button--login"
+              scroll={false}
+            >
+              Log in
+            </Link>
+            <Link
+              href="/signup"
+              className="nondashboard-navbar__auth-button--signup"
+              scroll={false}
+            >
+              Sign up
+            </Link>
+          </SignedOut>
         </div>
       </div>
     </nav>
