fc4383319f9b1d78607711ab4340b61111ac8265
monaabishek2003 <monaabisheka@gmail.com>
2025-01-03 16:54:44 +0530

    signin & signup page added feature/auth - 1

--- middleware.ts
+++ middleware.ts
@@ -6,17 +6,15 @@ const isTeacherRoute = createRouteMatcher(["/teacher/(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { sessionClaims } = await auth();
-  const userRole =
-    (sessionClaims?.metadata as { userType: "student" | "teacher" })
-      ?.userType || "student";
+  const userRole = (sessionClaims?.metadata as {userType: "student"|"teacher"})?.userType || "student";
 
+  // if(isStudentRoute(req)) if(userRole!="student") return NextResponse.redirect(new URL("/teacher/courses", req.url))
   if (isStudentRoute(req)) {
     if (userRole !== "student") {
       const url = new URL("/teacher/courses", req.url);
       return NextResponse.redirect(url);
     }
   }
-
   if (isTeacherRoute(req)) {
     if (userRole !== "teacher") {
       const url = new URL("/user/courses", req.url);
