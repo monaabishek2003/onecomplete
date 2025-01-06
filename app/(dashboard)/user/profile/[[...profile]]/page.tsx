2ef38ed40e1c9c6bb919cee78f1d8adea66f7f60
monaabishek2003 <monaabisheka@gmail.com>
2025-01-06 13:17:07 +0530

    developed appsidebar & Navbar feature/auth - 4

--- app/(dashboard)/user/profile/[[...profile]]/page.tsx
+++ app/(dashboard)/user/profile/[[...profile]]/page.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import { UserProfile } from '@clerk/nextjs';
-import Header from '@/components/Header';
+import Header from "@/components/Header";
+import { UserProfile } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import React from "react";
 
-
-const page = () => {
+const UserProfilePage = () => {
   return (
-    <div>
-      <Header title="Profile" subtitle="View your profile"/>
-      <UserProfile/>
-    </div>
-  )
-}
+    <>
+      <Header title="Profile" subtitle="View your profile" />
+      <UserProfile
+        path="/user/profile"
+        routing="path"
+        appearance={{
+          baseTheme: dark,
+          elements: {
+            scrollBox: "bg-customgreys-darkGrey",
+            navbar: {
+              "& > div:nth-child(1)": {
+                background: "none",
+              },
+            },
+          },
+        }}
+      />
+    </>
+  );
+};
 
-export default page;
+export default UserProfilePage;
